9caa03735b539e045a2bd83113206f7ddfe8cb2a
Arman Mir <mirarman8583@gmail.com>
2025-11-21 09:18:43 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 3000;
-require('dotenv').config()
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //  MiddleWare
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@kajwala.9fiaw1u.mongodb.net/?appName=kajwala`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World! zap is shifting shifting!");
