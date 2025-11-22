cf7498e1239b40dba5e63d0fa1a34e55ff95cc8d
Arman Mir <mirarman8583@gmail.com>
2025-11-22 08:54:58 +0600

    implemented get parcel and query email

--- index.js
+++ index.js
@@ -34,7 +34,20 @@ async function run() {
       // Parcel API
 
       app.get('/parcels', async(req, res) => {
-          
+        const query = {}
+        const {email } = req.query;
+
+        // /parcel?email=''&
+        
+        if (email) {
+          query.senderEmail = email;
+
+        }
+
+        
+        const cursor = parcelCollections.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
       })
 
       app.post('/parcels', async(req, res) => {
