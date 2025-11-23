ef8693a530281b8d68b16585214d4b38acf6b33e
Arman Mir <mirarman8583@gmail.com>
2025-11-23 09:10:44 +0600

    Stripe Payment GateWay API implemented

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+// stripe Payment GateWay
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
 //  MiddleWare
 
 app.use(express.json());
@@ -25,63 +28,83 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-      
-      
-      const db = client.db('zap_Shift_db')
-      const parcelCollections = db.collection('parcels');
+    const db = client.db("zap_Shift_db");
+    const parcelCollections = db.collection("parcels");
 
+    // Parcel API
 
-      // Parcel API
+    app.get("/parcels", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
 
-      app.get('/parcels', async(req, res) => {
-        const query = {}
-        const {email } = req.query;
+      // /parcel?email=''&
 
-        // /parcel?email=''&
-        
-        if (email) {
-          query.senderEmail = email;
+      if (email) {
+        query.senderEmail = email;
+      }
+      const options = { sort: { createdAt: -1 } };
 
-        }
-        const options = { sort : {createdAt: -1} }
+      const cursor = parcelCollections.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-        
-        const cursor = parcelCollections.find(query, options);
-        const result = await cursor.toArray();
-        res.send(result);
-      })
-
-    
-    
-    app.get('/parcels/:id', async (req, res) => {
+    app.get("/parcels/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
+      const query = { _id: new ObjectId(id) };
       const result = await parcelCollections.findOne(query);
-      res.send(result)
-    })
-    
-    
-    
-      app.post('/parcels', async(req, res) => {
-        const parcel = req.body;
-        // Parcel Created Time
-        parcel.createdAt = new Date();
-          const result = await parcelCollections.insertOne(parcel)
-          res.send(result)
-      })
-      
+      res.send(result);
+    });
 
-    app.delete('/parcels/:id',async  (req, res) => {
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      // Parcel Created Time
+      parcel.createdAt = new Date();
+      const result = await parcelCollections.insertOne(parcel);
+      res.send(result);
+    });
+
+    app.delete("/parcels/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      
+      const query = { _id: new ObjectId(id) };
+
       const result = await parcelCollections.deleteOne(query);
       res.send(result);
-    })
+    });
+
+    // Payment Related APIs
+
+    app.post("/create-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+            price_data: {
+              currency: 'USD',
+              unit_amount: amount,
+              product_data: {
+                name: paymentInfo.parcelName
+              }
+            },
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo.senderEmail,
+
+        mode: "payment",
+        metadata: {
+          parcelId: paymentInfo.parcelId
+        },
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+      });
+      console.log(session);
+      res.send({url: session.url})
       
-      
-      
-      
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
